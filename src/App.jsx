0ddc9435aa1e50de51731b0998b4a6dc379b79d8
Javier Ruedas <jruedas@twc.com>
2026-05-10 13:51:00 -0500

    display dropdowns correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,53 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Map from "./components/Map";
 import './css/App.css';
 import states from './data/mex-states.json';
 import Dropdown from "./components/Dropdown";
+import groupsByState from './data/state-groups.json';
 
 function App(){
     const [selectedState, setSelectedState] = useState("");
+    const [stateGroups, setStateGroups] = useState([]);
+
+    useEffect(() => {
+        const groups = groupsByState[selectedState] || [];
+
+         const newGroups = groups.map((group, i) => ({
+            ...group,
+            active: i === 0
+        }));
+
+        setStateGroups(newGroups);
+        }, [selectedState]);
+
 
     const handleChange = (e) => {
-        console.log(e.target.value);
         setSelectedState(e.target.value);
     }
 
+    const handleGroupChange = e => {
+        const code = e.target.value;
+        setStateGroups(prev => prev.map(group => ({
+            ...group,
+            active: group.code === code
+        })));
+    }
+
     return <div id="page-wrapper">
-       <Dropdown
-           options={states}
-           id="state"
-           selectedValue={selectedState}
-           onChange={handleChange}
-       />
+       <div id="dropdowns">
+           <Dropdown
+               options={states}
+               id="state"
+               selectedValue={selectedState}
+               onChange={handleChange}
+           />
+           {selectedState && <Dropdown
+                options={stateGroups}
+                id='group'
+                selectedValue={stateGroups.find(group => group.active)?.code || ""}
+                onChange={handleGroupChange}
+           />}
+       </div>
        <Map selectedState={selectedState} />
     </div>
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -3,13 +3,14 @@ import '../css/Dropdown.css';
 function Dropdown( {options, id, selectedValue, onChange }){
     return <div className='select-and-label'>
         <label htmlFor={id}>{id.charAt(0).toUpperCase() + id.slice(1)}:</label>
-        <select
-            className='dropdown' id={id}
-            onChange={onChange}
-            value={selectedValue}
-        >
-            {
-                options.map(option => (
+        <div className='select-wrapper'>
+            <select
+                className='dropdown' id={id}
+                onChange={onChange}
+                value={selectedValue}
+            >
+                {
+                    options.map(option => (
                         <option
                             value={option.code}
                             key={option.code}
@@ -17,8 +18,9 @@ function Dropdown( {options, id, selectedValue, onChange }){
                             {option.name}
                         </option>
                     ))
-            }
-        </select>
+                }
+            </select>
+        </div>
     </div>
 
 }
